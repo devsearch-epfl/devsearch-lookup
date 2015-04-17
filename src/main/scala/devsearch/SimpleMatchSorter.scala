dae0453ed26106bcbf893e8e398092be526c1ec8
mrudelle <matthieu.rudelle@gmail.com>
2015-04-17 04:02:01 +0200

    Put back nicolas' clustering technique

--- src/main/scala/devsearch/SimpleMatchSorter.scala
+++ src/main/scala/devsearch/SimpleMatchSorter.scala
@@ -10,10 +10,33 @@ object SimpleMatchSorter {
 
   def sort(groupedFeatures: RDD[(Location, Iterable[FeatureData])], withRanking: Boolean = true)(implicit sc: SparkContext): RDD[(Location, Int)] = {
 
+    def clamp(x: Double, min: Double, max: Double): Double = if (x < min) min else if (x > max) max else x
+
+
     // sorts by number of feature per location
-    groupedFeatures.sortBy(_._2.size).map{
-      case (location, features) => (location, features.head.line);
-    }
+    groupedFeatures.flatMap{
+      case (location, features) =>
+
+        // TODO: Cluster epsilon should maybe depend on the language of the file?
+        //       Typically scala features will be much closer to each other than in Java...
+        val positions = features.map(_.line).toArray
+        val clusters = DBSCAN(positions, 5.0, positions.length min 3)
+
+        clusters.map { cluster =>
+          val key:(Location, Int) = location -> cluster.min
+
+          val size = cluster.size
+          val radius = (cluster.max - cluster.min) / 2.0 + 1 // avoid radius = 0
+          val densityScore = clamp(size / radius, 0, 5) / 5.0
+
+          val sizeScore = clamp(size, 0, 20) / 20.0
+
+          val finalScore = .6 * densityScore + .4 * sizeScore
+
+          key -> finalScore
+
+        }
+    }.sortBy(-_._2).map(_._1)
 
   }
 }
