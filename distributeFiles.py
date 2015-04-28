9b8175c08039dfc1fa75326408e27dfe6710949a
hubi <nicolas.hubacher@epfl.ch>
2015-04-29 00:14:12 +0200

    weights can be assigned to buckets. updated instructions for use.

--- distributeFiles.py
+++ distributeFiles.py
@@ -3,6 +3,13 @@
 # This script applies 'consistent hashing' to our features. More about consistent hashing on
 # http://www.tom-e-white.com//2007/11/consistent-hashing.html
 # 
+#
+# Usage: 
+# - first argument is the input json file containing the features
+# - the other arguments are at least one path or tuple: bucket_x.json weight_x 
+#   (weight_x is optional)
+#
+#
 # installing the hash_ring module: 'sudo easy_install hash_ring'
 # see http://amix.dk/blog/viewEntry/19367
 
@@ -17,27 +24,43 @@ if(len(args) < 3):
 	raise Exception('''
 \tNot enough arguments! Correct usage: 
 \t - arg1 = path/to/features.json
-\t - arg2, ..., argN = path/to/output.json''')
+\t - arg2, ..., argN = path/to/output.json optionalWeight
+''')
 	sys.exit(0)
 
 source  = args[1]
 buckets = []
+weights = {}
 
 if(not os.path.isfile(source)):
 	raise Exception('''
 \tInput directory does not exist! Correct usage:
 \t - arg1 = path/to/features.json
-\t - arg2, ..., argN = path/to/output.json''')
+\t - arg2, ..., argN = path/to/output.json optionalWeight
+''')
 	sys.exit(0)
 
 
-for output in args[2::]:
-	buckets.append(open(output, 'w+'))
+i = 2
+while i < len(args):
+	f = open(args[i], 'w+')
+	buckets.append(f)
 
+	if(i < len(args)-1 and args[i + 1].isdigit()):
+		weights[f] = int(args[i + 1])
+		i = i + 2
+	else:
+		weights[f] = 1
+		i = i + 1
+
+
+
+
+print buckets
 
 
 # create hash ring
-ring = HashRing(buckets)
+ring = HashRing(buckets, weights)
 
 nbFiles = 0
 
@@ -49,10 +72,10 @@ with open(source) as f:
 		bucket = ring.get_node(j['file'])
 		bucket.write(line)
 		if(nbFiles%500000 == 0):
-			print 'Processed ' + str(nbFiles/1000) + 'k features.' 
+			print 'Processed ' + str(nbFiles/1000) + 'k features' 
 	
 
-print 'Total: ' + str(nbFiles) + ' features distributed. \n'
+print 'Totally ' + str(nbFiles) + ' features distributed \n'
 
 for output in buckets:
 	output.close()
