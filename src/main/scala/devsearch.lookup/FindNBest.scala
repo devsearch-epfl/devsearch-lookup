23a40ce528f70b8078131ea302ee44a3b3b69067
Mateusz Gołębiewski <matix2267@gmail.com>
2015-04-29 16:31:23 +0200

    Forgot to add dummy implementation in previous commit, now it is working

---
+++ src/main/scala/devsearch.lookup/FindNBest.scala
@@ -0,0 +1,20 @@
+package devsearch.lookup
+
+import java.util.Random
+
+/**
+ * QuickSelect algorithm
+ * http://en.wikipedia.org/wiki/Quickselect
+ */
+object FindNBest {
+  val rand = new Random(System.currentTimeMillis())
+
+  def apply(list: Seq[SearchResultEntry], n: Int): Seq[SearchResultEntry] = {
+    if (n <= 0) return Seq()
+    if (list.size <= n) return list
+    if (list.size <= 5) return list.sortBy(_.score).takeRight(n)
+    val pivot = list(rand.nextInt(list.size)).score
+    val (left, right) = list.partition(_.score >= pivot)
+    apply(left, n) ++ apply(right, n - left.size)
+  }
+}
