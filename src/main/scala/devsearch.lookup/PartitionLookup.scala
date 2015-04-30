694b27c3bfa778066e5a3040b7e456fb609c1d4e
hubi <nicolas.hubacher@epfl.ch>
2015-04-30 20:27:18 +0200

    Very basic scoring function.

--- src/main/scala/devsearch.lookup/PartitionLookup.scala
+++ src/main/scala/devsearch.lookup/PartitionLookup.scala
@@ -20,17 +20,13 @@ class PartitionLookup extends Actor with ActorLogging {
   val db = connection("devsearch")
   val collection = db("features")
 
-  def getScore(repo: String, file: String, line: Int): Float = {
-    line // TODO we need some scoring function here, for now just anything that has proper type
-  }
-
   def getMatchesFromDb(features: Seq[String]): Future[SearchResult] = {
     if (features.isEmpty) Future.successful(SearchResultSuccess(Seq()))
     else {
       val query = BSONDocument("$or" -> BSONArray(
         features.map(f => BSONDocument("feature" -> f))
       ))
-      val filter = BSONDocument("_id" -> 0, "file" -> 1, "line" -> 1)
+      val filter = BSONDocument("_id" -> 0, "file" -> 1, "line" -> 1, "feature" -> 1)
       val matchesFuture: Future[List[BSONDocument]] =
         collection.find(query, filter).cursor[BSONDocument].collect[List](10)
       matchesFuture.map{ list =>
@@ -38,12 +34,18 @@ class PartitionLookup extends Actor with ActorLogging {
           val repoAndFile = doc.getAs[String]("file").get
           val firstSlash = repoAndFile.indexOf("/")
           val secondSlash = repoAndFile.indexOf("/", firstSlash + 1)
-          val repo = repoAndFile.substring(0, secondSlash)
+          val owner = repoAndFile.substring(0, firstSlash)
+          val repo = repoAndFile.substring(firstSlash + 1, secondSlash)
           val file = repoAndFile.substring(secondSlash + 1)
           val line = doc.getAs[Long]("line").get.toInt
-          val score = getScore(repo, file, line)
-          SearchResultEntry(repo, file, line, score)
-        }, 10))
+          //val score = getScore(repo, file, line)
+
+          //get feature type and name
+          val feature = doc.getAs[String]("feature").get
+
+
+          new FeatureEntry(Location(owner, repo, file), feature, line)
+        }.groupBy(_.loc).map(toSearchResultEntry).toSeq, 10))
       }
     }
   }
@@ -59,4 +61,32 @@ class PartitionLookup extends Actor with ActorLogging {
       result pipeTo sender
     case x => log.error(s"Received unexpected message $x")
   }
+
+
+  /**
+   * Generates superfeatures of each file and returns a searchResultEntry.
+   * @param entry
+   * @return
+   */
+  def toSearchResultEntry(entry: (Location, List[FeatureEntry])): SearchResultEntry = entry match{
+    case (Location(owner, repo, file), list) => {
+      val nbMatches = list.length
+
+
+      val score = 1 * nbMatches
+
+
+      print("\n\n\t"+owner+" score: "+score+"\n\nS")
+
+
+
+      SearchResultEntry(owner+"/"+repo, file, 0, score.toFloat)
+
+    }
+  }
 }
+
+
+
+case class Location(owner: String, repo: String, file: String)
+case class FeatureEntry(loc: Location, feature: String, line: Int)
