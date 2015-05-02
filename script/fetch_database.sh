0a650ee57057601eb92eef30a0928f2744107760
Bastien Jacot-Guillarmod <bjacotg@gmail.com>
2015-05-02 14:42:06 +0200

    I is stoopidzzz

--- script/fetch_database.sh
+++ script/fetch_database.sh
@@ -4,10 +4,10 @@ set -e
 
 sudo apt-get -y install mongodb
 
-# Fetch features files TODO
+# Fetch features files should be here from the start
 
-# Convert the database ???TODO???
-
+# Convert the database
+python features2json.py < feature.raw > features.json
 # Import the file in the database
 
 mongoimport --db devsearch --collection features --file features.json --drop
