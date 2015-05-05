3ca6877a868d68faca62be2f654914a1b75cc0ee
Bastien Jacot-Guillarmod <bjacotg@gmail.com>
2015-05-05 15:16:49 +0200

    Sorry...

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,8 +9,6 @@ wget https://dl.bintray.com/sbt/debian/sbt-0.13.7.deb
 sudo dpkg -i sbt-0.13.7.deb
 
 
-# clone our Play app
+# clone akka cluster process
 git clone https://github.com/devsearch-epfl/devsearch-lookup.git
-cd devsearch-lookup
-git checkout deploy
 echo '*** Setup is complete ***'
