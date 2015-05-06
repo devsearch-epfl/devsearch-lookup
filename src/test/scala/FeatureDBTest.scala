c5d00c5124a213b2a17cc4dc01f518e3aa7abb50
mrudelle <matthieu.rudelle@gmail.com>
2015-05-06 13:07:31 +0200

    test improved to use dummy data

--- src/test/scala/FeatureDBTest.scala
+++ src/test/scala/FeatureDBTest.scala
@@ -7,11 +7,20 @@ import scala.concurrent.duration.Duration
 
 class ExampleSpec extends FlatSpec with Matchers {
 
-  "The FeatureDB" should "return the correct amount of result" in {
-    val results = FeatureDB.getMatchesFromDb(Seq("className=ExampleModule", "typeReference=Type", "variableDeclaration=connectorId type=String", "variableDeclaration=type type=Type"))
+  "The FeatureDB" should "return 2 times 5 hits" in {
+    val results = FeatureDB.getMatchesFromDb(Seq("dummyfeature1", "dummyfeature2", "dummyfeature3", "dummyfeature4", "dummyfeature5"))
     val list = Await.result(results, Duration.Inf)
 
-    list.size should be (99)
+    list.size should be (2)
+    list.map{ docHit => docHit.hits.length}.toList should be (List(5,5))
+  }
+
+  it should "return the 2 times 2 hits" in {
+    val results = FeatureDB.getMatchesFromDb(Seq("dummyfeature1", "dummyfeature5"))
+    val list = Await.result(results, Duration.Inf)
+
+    list.size should be (2)
+    list.map{ docHit => docHit.hits.length}.toList should be (List(2,2))
   }
 
   it should "return an empty stream when there is no match" in {
