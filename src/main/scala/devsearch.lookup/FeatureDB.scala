d21e1c17265ec4bd79ae3a7fe3c43214426e06f5
mrudelle <matthieu.rudelle@gmail.com>
2015-05-06 10:27:24 +0200

    fixed big mistake

--- src/main/scala/devsearch.lookup/FeatureDB.scala
+++ src/main/scala/devsearch.lookup/FeatureDB.scala
@@ -44,7 +44,7 @@ object FeatureDB {
         BSONDocument(
           "$match" -> BSONDocument(
             "feature" -> BSONDocument(
-              "$in" -> "features"))),
+              "$in" -> features))),
         BSONDocument(
           "$group" -> BSONDocument(
             "_id" -> "$file",
