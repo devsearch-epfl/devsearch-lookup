283857832d156bdeb1a9c6d66ab0061a06a7f7d2
Mateusz Gołębiewski <matix2267@gmail.com>
2015-05-07 15:14:43 +0200

    rename FindNBestNew => FindNBest and deprecate old one

--- src/test/scala/FindNBestTest.scala
+++ src/test/scala/FindNBestTest.scala
@@ -1,4 +1,4 @@
-import devsearch.lookup.FindNBestNew
+import devsearch.lookup.FindNBest
 import org.scalatest._
 
 class FindNBestTest extends FlatSpec with Matchers {
@@ -7,28 +7,28 @@ class FindNBestTest extends FlatSpec with Matchers {
 
   "FindNBest" should "return the requested number of matches" in {
     val input = Stream(1,2,3,4,5)
-    FindNBestNew(input, int2float, 3).length shouldEqual 3
+    FindNBest(input, int2float, 3).length shouldEqual 3
   }
 
   it should "work if we request too many results" in {
     val input = Stream(1,2,3)
-    FindNBestNew(input, int2float, 100).length shouldEqual 3
+    FindNBest(input, int2float, 100).length shouldEqual 3
   }
 
   it should "return only best results" in {
     val input = Stream(1,2,3,4,5)
-    FindNBestNew(input, int2float, 1) shouldEqual List(5)
+    FindNBest(input, int2float, 1) shouldEqual List(5)
   }
 
   it should "work correctly when multiple items have same score" in {
     val input = Stream(('a',0), ('b',0), ('c',1), ('d',1))
-    val result = FindNBestNew[(Char,Int)](input, _._2, 3)
+    val result = FindNBest[(Char,Int)](input, _._2, 3)
     result should contain ('c',1)
     result should contain ('d',1)
   }
 
   it should "return results sorted" in {
     val input = Stream(3,5,1,2,4)
-    FindNBestNew(input, int2float, 5) shouldEqual List(5,4,3,2,1)
+    FindNBest(input, int2float, 5) shouldEqual List(5,4,3,2,1)
   }
 }
