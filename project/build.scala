8d0ef64266ca621b3618b185ef1f26fcbac58a82
Mateusz Gołębiewski <matix2267@gmail.com>
2015-05-08 17:00:51 +0200

    trying to import stuff

--- project/build.scala
+++ project/build.scala
@@ -2,8 +2,10 @@ import sbt._
 
 object MyBuild extends Build{
 
-  lazy val root = Project("root", file(".")).dependsOn(astProject)
+  lazy val root = Project("root", file(".")).dependsOn(astProject).dependsOn(learningProject)
   lazy val astProject = RootProject(uri("git://github.com/devsearch-epfl/devsearch-ast.git#" + astProjectCommit))
   lazy val astProjectCommit = "8b028a9ac78b01680a22994b08aef1436ac1382b"
+  lazy val learningProject = RootProject(uri("git://github.com/devsearch-epfl/devsearch-learning.git#" + learningProjectCommit))
+  lazy val learningProjectCommit = "547a3642315c1afae4076409bbe5bf23fa8df73b"
 
 }
