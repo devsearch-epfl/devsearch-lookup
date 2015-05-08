50b5f38242d0a9698753c10f33d8a653e6a88e73
mrudelle <matthieu.rudelle@gmail.com>
2015-05-08 04:51:30 +0200

    added scoreBreakdown and featureList

--- src/main/scala/devsearch.lookup/Messages.scala
+++ src/main/scala/devsearch.lookup/Messages.scala
@@ -6,4 +6,4 @@ sealed trait SearchResult
 case class SearchResultSuccess(entries: Seq[SearchResultEntry], count: Long) extends SearchResult
 case class SearchResultError(error: String) extends SearchResult
 
-case class SearchResultEntry(user: String, repo: String, path: String, lineStart: Int, lineEnd: Int, score: Float)
+case class SearchResultEntry(user: String, repo: String, path: String, lineStart: Int, lineEnd: Int, score: Float, scoreBreakDown: Map[String, Double], featureList: Set[String])
