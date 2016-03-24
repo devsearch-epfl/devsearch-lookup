2abfc729b5f8e7dc16c9f8d42d52e61ee85a6df8
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2016-03-24 15:14:10 +0100

    fix

--- deploy_setup.sh
+++ deploy_setup.sh
@@ -5,7 +5,7 @@ mkdir bucket-data
 echo "starting installation with mongodb and sbt"
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list$
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt.list
 sudo apt-get update
