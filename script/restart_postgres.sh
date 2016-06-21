733c330c702043ef9436e3a681f06d41f1a84d28
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2016-06-21 17:06:18 +0200

    wait for server stop

--- script/restart_postgres.sh
+++ script/restart_postgres.sh
@@ -2,10 +2,13 @@
 set -e
 
 
-sudo service postgresql stop
-
 # create directory where the db will lie
 db_path="/var/postgresql/devsearch/"
+
+
+# stop postgresql as postgres user
+sudo su -c "/usr/lib/postgresql/*/bin/pg_ctl -D ${db_path} -l ${db_path}/logfile start -w" postgres
+
 
 # start postgresql as postgres user
 sudo su -c "/usr/lib/postgresql/*/bin/pg_ctl -D ${db_path} -l ${db_path}/logfile start -w" postgres
