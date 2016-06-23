a2a4be96f69562390fa06d933b510e6202d00da5
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2016-06-23 22:24:56 +0200

    hot fix

--- script/restart_postgres.sh
+++ script/restart_postgres.sh
@@ -7,7 +7,7 @@ db_path="/var/postgresql/devsearch/"
 
 
 # stop postgresql as postgres user
-sudo su -c "/usr/lib/postgresql/*/bin/pg_ctl -D ${db_path} -l ${db_path}/logfile start -w" postgres
+sudo su -c "/usr/lib/postgresql/*/bin/pg_ctl -D ${db_path} -l ${db_path}/logfile stop -w" postgres
 
 
 # start postgresql as postgres user
